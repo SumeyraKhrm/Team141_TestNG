66c3e5157ef85bbe4d2a5049122eaad81b3c551a
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-06-02 14:47:01 +0200

    version

--- src/test/java/tests/day17_testNGFramework/C04_DriverClassIlkTest.java
+++ src/test/java/tests/day17_testNGFramework/C04_DriverClassIlkTest.java
@@ -1,4 +1,23 @@
 package tests.day17_testNGFramework;
 
+import org.testng.annotations.Test;
+import utilities.Driver;
+
 public class C04_DriverClassIlkTest {
+
+
+    // TextNG Page Object Model'de
+    // bizim elimiz kolumuz, gozumuz olan webdriver objesi olusturmak icin
+    // Driver class'indaki static getDriver() kullanilir
+
+    @Test
+    public void test01() throws InterruptedException {
+
+        Driver.getdriver().get("https://www.testotomasyonu.com");
+
+        Thread.sleep(3);
+
+        Driver.quitDriver();
+
+    }
 }
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,39 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Driver {
+
+
+    // Driver class'i WebDriver objesini olusturacagimiz
+    // ve gerkeli ayarlari yapacagimiz class olacak
+
+    // Hedef configuration properties'den hangi browser secilirse
+    // bize o browser'i uretecek bir class olusturmak
+
+    // Driver class'inda bize driver objesi dondurecek getDriver() olusturacagiz
+
+    public static WebDriver driver ;
+
+
+    public static WebDriver getdriver(){
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        return driver;
+
+    }
+
+    public static void quitDriver(){
+        driver.quit();
+
+    }
+
+
+}
