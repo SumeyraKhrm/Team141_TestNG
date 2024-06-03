8691f8506362411b018872241ff56e4cb90e165a
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-06-03 15:03:33 +0200

    version

---
+++ src/test/java/pages/FacebookPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class FacebookPage {
+
+
+         public FacebookPage(){
+            PageFactory.initElements(Driver.getdriver(),this);
+        }
+
+
+
+     @FindBy (id="email")
+    public WebElement emailKutusu;
+
+    @FindBy(id = "pass")
+    public WebElement passwordKutusu;
+
+    @FindBy(name = "login")
+    public WebElement loginButonu;
+
+
+    @FindBy(xpath = "(//*[text()='Tüm çerezlere izin ver'])[2]")
+    public WebElement cookiesKabulButonu;
+
+
+    @FindBy(className = "_9ay7")
+    public WebElement basariliGirisYapilamadiUyarisi;
+
+
+
+}
