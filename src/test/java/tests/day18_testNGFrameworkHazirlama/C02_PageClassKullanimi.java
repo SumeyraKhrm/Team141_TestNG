5a8b2d356c009846aa30ad73cecb2a6e2c1c755e
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-06-03 10:52:38 +0200

    version

--- src/test/java/tests/day18_testNGFrameworkHazirlama/C02_PageClassKullanimi.java
+++ src/test/java/tests/day18_testNGFrameworkHazirlama/C02_PageClassKullanimi.java
@@ -1,4 +1,37 @@
 package tests.day18_testNGFrameworkHazirlama;
 
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.Driver;
+
 public class C02_PageClassKullanimi {
+
+
+    @Test
+    public void test01(){
+
+        // testotomasyonu sayfasina gidin
+        Driver.getdriver().get("https://www.testotomasyonu.com");
+
+        // phone icin arama yapin
+
+        // POM'de Page class'larindaki locate'leri kullanmak icin
+        // page class'larindan Obje Olusturma yontemi benimsenmistir
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();  //obje olusturduk
+        testotomasyonuPage.aramakutusu.sendKeys("phone" + Keys.ENTER);
+
+        // arama sonucunda urun bulunabildigini test edin
+        String unExpectedAramaSonucu = "0 Products Found";
+        String actualAramaSonucu = testotomasyonuPage.aramaSonucElementi.getText();
+
+        Assert.assertNotEquals(actualAramaSonucu,unExpectedAramaSonucu);
+
+        // sayfayi kapatin
+        Driver.quitDriver();
+
+
+    }
+
 }
