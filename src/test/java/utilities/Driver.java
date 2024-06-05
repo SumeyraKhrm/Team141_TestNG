83f9ee0b054ded0f2b8600030c115387cef20437
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-06-05 22:22:41 +0200

    version

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -2,6 +2,9 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 import java.util.List;
@@ -23,12 +26,32 @@ public class Driver {
     public static WebDriver getdriver(){
 
         if( driver == null){    //her seferinde yeni sayfa acılmasını istemiyoruz
+                String istenenBrowser = ConfigReader.getProperty("browser"); // firefox
+
+                switch (istenenBrowser){
+
+                    case "firefox" :
+                        driver = new FirefoxDriver();
+                        break;
+
+                    case "edge" :
+                        driver = new EdgeDriver();
+                        break;
+
+                    case "safari" :
+                        driver = new SafariDriver();
+                        break;
+
+                    default:
+                        driver = new ChromeDriver();
+                }
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         }
         return driver;
-
     }
 
     public static void quitDriver(){
@@ -36,7 +59,4 @@ public class Driver {
         driver = null;      //kapattıktan sonrada yeni chromedriver atanabilmesi için null degeri atıyoruz
 
     }
-
-
-
 }
