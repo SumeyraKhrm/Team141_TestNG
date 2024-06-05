8e8bbcd0b95321f31eaf10f6b34e82ddf1e04327
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-06-05 20:54:23 +0200

    version

--- src/test/java/pages/TestotomasyonuPage.java
+++ src/test/java/pages/TestotomasyonuPage.java
@@ -14,14 +14,12 @@ public class TestotomasyonuPage {
     // constructor icinde driver'i class'a tanimlariz
 
 
-
     public TestotomasyonuPage(){
         PageFactory.initElements(Driver.getdriver(),this);
     }
 
     // WebElement aramakutusu = Driver.getDriver().findElement(By.id("global-search"));
     // List<WebElement> sonucElementleriList = Driver.getDriver().findElements(By.className("prod-img"));
-
 
     @FindBy(id ="global-search")
     public
@@ -30,8 +28,35 @@ public class TestotomasyonuPage {
     @FindBy(className = "product-count-text")
     public WebElement aramaSonucElementi;
 
-
     @FindBy(className = "prod-img")
     public List<WebElement> sonucElementleriList;  //bir liste getircekse boyle kayıt ediyoruz
+
+    @FindBy(xpath = "//span[text()='Account']")
+    public WebElement accountLinki;
+
+    @FindBy(id = "email")
+    public WebElement loginEmailKutusu;
+
+    @FindBy(id = "password")
+    public WebElement loginPasswordKutusu;
+
+    @FindBy(id = "submitlogin")
+    public WebElement loginSiginButonu;
+
+    @FindBy(xpath = "//span[text()='Logout']")
+    public WebElement logOutButonu;
+
+    @FindBy(xpath = "//div[@class=' heading-sm mb-4']")
+    public WebElement ilkUrunSayfasindaUrunIsimElementi;
+
+    @FindBy(xpath = "//*[@class='add-to-cart']")
+    public WebElement addToCartButonu;
+
+    @FindBy(xpath = "(//span[@class='menu-icon-text'])[3]")
+    public WebElement yourCartLinki;
+
+    @FindBy(xpath = "//*[@*='product-title text-center']")
+    public WebElement sepettekiUrunIsimElementi;
+
 
 }
---
+++ src/test/java/tests/day19_testNGFramework_assertions/C03_NegatifLoginTesti.java
@@ -0,0 +1,79 @@
+package tests.day19_testNGFramework_assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C03_NegatifLoginTesti {
+
+
+    //1- https://www.testotomasyonu.com/ anasayfasina gidin
+    //2- account linkine basin
+    //3-  3 farkli test method’u olusturun.
+    //	- gecerli email, gecersiz password
+    //	- gecersiz email, gecerli password
+    //	- gecersiz email, gecersiz password.
+    //4- Login butonuna basarak login olmayi deneyin
+    //5- Basarili olarak giris yapilamadigini test edin
+
+    @Test
+      public void NegatifLoginTesti(){
+
+        Driver.getdriver().get(ConfigReader.getProperty("toUrl"));
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+        testotomasyonuPage.loginEmailKutusu.sendKeys(ConfigReader.getProperty("toGecerliEmail"));
+        testotomasyonuPage.loginPasswordKutusu.sendKeys(ConfigReader.getProperty("toGecersizPassword"));
+        testotomasyonuPage.loginSiginButonu.click();
+        Assert.assertTrue(testotomasyonuPage.loginSiginButonu.isDisplayed());
+        //sign in butonu hala görünüyorsa basarılı bir giris yapamadım demektir
+        Driver.quitDriver();
+
+
+    }
+
+    @Test
+    public void gecersizEmailTesti(){
+
+        Driver.getdriver().get(ConfigReader.getProperty("toUrl"));
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+        testotomasyonuPage.loginEmailKutusu.sendKeys(ConfigReader.getProperty("toGecersizEmail"));
+        testotomasyonuPage.loginPasswordKutusu.sendKeys(ConfigReader.getProperty("toGecerliPassword"));
+        testotomasyonuPage.loginSiginButonu.click();
+        Assert.assertTrue(testotomasyonuPage.loginSiginButonu.isDisplayed());
+        //sign in butonu hala görünüyorsa basarılı bir giris yapamadım demektir
+        Driver.quitDriver();
+
+    }
+
+
+    @Test
+    public void gecersizEmailGecersizPasswordTesti(){
+
+        //1- https://www.testotomasyonu.com/ anasayfasina gidin
+        Driver.getdriver().get(ConfigReader.getProperty("toUrl"));
+
+        //2- account linkine basin
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        testotomasyonuPage.accountLinki.click();
+
+        //3- gecersiz email, gecersiz password girin
+        testotomasyonuPage.loginEmailKutusu.sendKeys(ConfigReader.getProperty("toGecersizEmail"));
+        testotomasyonuPage.loginPasswordKutusu.sendKeys(ConfigReader.getProperty("toGecersizPassword"));
+
+        //4- Login butonuna basarak login olmayi deneyin
+        testotomasyonuPage.loginSiginButonu.click();
+
+        //5- Basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(testotomasyonuPage.loginEmailKutusu.isDisplayed());
+        ReusableMethods.bekle(3);
+        //6- Sayfayi kapatin
+        Driver.quitDriver();
+
+    }
+
+}
